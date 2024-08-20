1a629b28d2989ddad88eea1a64faeadd5686e1e3
Uyai-Abasi Isidore <isidoreuyaiabasi@gmail.com>
2024-08-20 13:01:11 +0000

    Initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,11 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+module.exports = {
+  solidity: "0.8.19",
+  networks: {
+    swisstronik: {
+      url: "https://json-rpc.testnet.swisstronik.com/",
+      accounts: ["special pole answer anchor client burger nut service stick sister scissors drum"],
+    },
+  },
+};
